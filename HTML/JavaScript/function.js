72d7086da4137fbfdcfd0b44c9ae357199f0b137
Emanuel Nieva <emmaxi22@gmail.com>
2023-02-18 19:23:39 -0300

    a

--- HTML/JavaScript/function.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('boton').onclick = function () {
-    console.log("Se realizo un click");
-    document.getElementById('demo').innerHTML = "Esto es una prueba de eventos en JS";
-}
